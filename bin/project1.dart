6cd6ab0b827c3d2adc544c69589f440f718927c6
PuteraBhagas <p.bhagaswara@gmail.com>
2025-09-19 10:41:00 +0700

    constanta dan final

--- bin/project1.dart
+++ bin/project1.dart
@@ -1,4 +1,5 @@
 import 'package:project1/project1.dart' as project1;
+const String nama = 'Putera';
 
 void main(List<String> arguments) {
  // print('Hello world: ${project1.calculate()}!');
@@ -7,4 +8,7 @@ void main(List<String> arguments) {
  multi line comment
  banyak line comment
  */
+//constanta dan final
+final String nama2 = 'Bhagas';
+print("Nama saya $nama $nama2");
 }
